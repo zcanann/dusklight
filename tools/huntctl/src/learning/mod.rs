8b15abffea6e743a228f83eef970f254d6644228
Zachary Canann <zacharycanann@gmail.com>
2026-07-18 11:33:55 -0700

    learning: add fixed semantic model representation

--- tools/huntctl/src/learning/mod.rs
+++ tools/huntctl/src/learning/mod.rs
@@ -7,6 +7,7 @@ pub mod double_q;
 pub mod ensemble_q;
 pub mod evaluation_isolation;
 pub mod factorized_actions;
+pub mod fixed_representation;
 pub mod fqi;
 pub mod goal_conditioning;
 pub mod hindsight;
